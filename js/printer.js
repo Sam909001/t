ce46090bf5e99c3c0269af6c7b8bce497fbe9a9d
Sam909001 <ademkaya1990kaya@gmail.com>
2025-09-24 03:49:08 +0300

    Update printer.js

--- js/printer.js
+++ js/printer.js
@@ -167,13 +167,13 @@ class PrinterServiceElectronWithSettings extends PrinterServiceElectron {
             const style = `
                 <style>
                 @page {
-                    size: 110mm 100mm landscape;
+                    size: 100mm 80mm portrait;
                     margin: 0;
                 }
 
                 body {
-                    width: 110mm;
-                    height: 100mm;
+                    width: 100mm;
+                    height: 80mm;
                     margin: 0;
                     padding: 0;
                     overflow: hidden;
