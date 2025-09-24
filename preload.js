b20f412f1366d84d5fc25c779cefb12f07fbd3f1
Sam909001 <ademkaya1990kaya@gmail.com>
2025-09-25 02:31:50 +0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,5 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  printBarcode: (htmlContent) => ipcRenderer.send('print-barcode', htmlContent)
+    printBarcode: (htmlContent) => ipcRenderer.invoke('print-barcode', htmlContent)
 });
