b9edbdc710566dde93252344f15c2712c2e7405f
Sam909001 <ademkaya1990kaya@gmail.com>
2025-09-27 02:00:25 +0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,20 +1,29 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    // Printing
-    printBarcode: (htmlContent) => ipcRenderer.invoke('print-barcode', htmlContent),
-    onPrintError: (callback) => ipcRenderer.on('print-error', (event, error) => callback(error)),
-
-    // Per-monitor ID
+    // --- Monitor ID ---
     setMonitorId: (callback) => ipcRenderer.on('set-monitor-id', (event, id) => callback(id)),
 
-    // SQLite / data fetching APIs
-    fetchPackagesForContainers: (containerIds) => ipcRenderer.invoke('fetchPackagesForContainers', containerIds),
-    getContainersCount: (monitorId, filter) => ipcRenderer.invoke('getContainersCount', monitorId, filter),
-    fetchCustomers: (monitorId) => ipcRenderer.invoke('fetch-customers', monitorId),
-    fetchPersonnel: (monitorId) => ipcRenderer.invoke('fetch-personnel', monitorId),
-    fetchPackages: (monitorId) => ipcRenderer.invoke('fetch-packages', monitorId),
-    calculateTotalQuantity: (packageIds) => ipcRenderer.invoke('calculate-total-quantity', packageIds),
-    fetchContainerDetails: (containerId, monitorId) => ipcRenderer.invoke('fetch-container-details', containerId, monitorId),
-    fetchStockItems: (monitorId) => ipcRenderer.invoke('fetch-stock-items', monitorId)
+    // --- Packages ---
+    fetchPackagesForContainers: ({ containerIds, monitorId }) =>
+        ipcRenderer.invoke('fetchPackagesForContainers', { containerIds, monitorId }),
+    fetchContainerDetails: ({ containerId, monitorId }) =>
+        ipcRenderer.invoke('fetchContainerDetails', { containerId, monitorId }),
+
+    // --- Shipping / Orders ---
+    getContainersCount: ({ monitorId, filter }) =>
+        ipcRenderer.invoke('getContainersCount', { monitorId, filter }),
+    fetchShippingPackages: ({ monitorId, filter }) =>
+        ipcRenderer.invoke('fetchShippingPackages', { monitorId, filter }),
+
+    // --- Stock Items ---
+    fetchStockItems: ({ monitorId, status }) =>
+        ipcRenderer.invoke('fetchStockItems', { monitorId, status }),
+
+    // --- Print Barcode / Labels ---
+    printBarcode: (htmlContent) => ipcRenderer.send('printBarcode', htmlContent),
+    printLabel: (htmlContent) => ipcRenderer.send('printLabel', htmlContent),
+
+    // --- Misc ---
+    showNotification: (title, body) => ipcRenderer.send('showNotification', { title, body }),
 });
