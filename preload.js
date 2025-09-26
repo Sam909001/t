3e124492c684f358e23f777bcafcfcb66f25d1cd
Sam909001 <ademkaya1990kaya@gmail.com>
2025-09-27 01:15:45 +0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,13 +1,20 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  printBarcode: (htmlContent) => ipcRenderer.invoke('print-barcode', htmlContent),
-  onPrintError: (callback) => ipcRenderer.on('print-error', (event, error) => callback(error)),
+    // Printing
+    printBarcode: (htmlContent) => ipcRenderer.invoke('print-barcode', htmlContent),
+    onPrintError: (callback) => ipcRenderer.on('print-error', (event, error) => callback(error)),
 
-  // NEW: receive unique monitor ID from main process
-  setMonitorId: (callback) => ipcRenderer.on('set-monitor-id', (event, id) => callback(id)),
+    // Per-monitor ID
+    setMonitorId: (callback) => ipcRenderer.on('set-monitor-id', (event, id) => callback(id)),
 
-  // optional: invoke SQLite IPC calls
-  fetchPackagesForContainers: (containerIds) => ipcRenderer.invoke('fetchPackagesForContainers', containerIds),
-  getContainersCount: (monitorId, filter) => ipcRenderer.invoke('getContainersCount', monitorId, filter),
+    // SQLite / data fetching APIs
+    fetchPackagesForContainers: (containerIds) => ipcRenderer.invoke('fetchPackagesForContainers', containerIds),
+    getContainersCount: (monitorId, filter) => ipcRenderer.invoke('getContainersCount', monitorId, filter),
+    fetchCustomers: (monitorId) => ipcRenderer.invoke('fetch-customers', monitorId),
+    fetchPersonnel: (monitorId) => ipcRenderer.invoke('fetch-personnel', monitorId),
+    fetchPackages: (monitorId) => ipcRenderer.invoke('fetch-packages', monitorId),
+    calculateTotalQuantity: (packageIds) => ipcRenderer.invoke('calculate-total-quantity', packageIds),
+    fetchContainerDetails: (containerId, monitorId) => ipcRenderer.invoke('fetch-container-details', containerId, monitorId),
+    fetchStockItems: (monitorId) => ipcRenderer.invoke('fetch-stock-items', monitorId)
 });
