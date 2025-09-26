30277e7abddcef9518992e8e5af39d0bc06079e0
Sam909001 <ademkaya1990kaya@gmail.com>
2025-09-27 01:12:05 +0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,7 +1,13 @@
-//
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   printBarcode: (htmlContent) => ipcRenderer.invoke('print-barcode', htmlContent),
-  onPrintError: (callback) => ipcRenderer.on('print-error', (event, error) => callback(error))
+  onPrintError: (callback) => ipcRenderer.on('print-error', (event, error) => callback(error)),
+
+  // NEW: receive unique monitor ID from main process
+  setMonitorId: (callback) => ipcRenderer.on('set-monitor-id', (event, id) => callback(id)),
+
+  // optional: invoke SQLite IPC calls
+  fetchPackagesForContainers: (containerIds) => ipcRenderer.invoke('fetchPackagesForContainers', containerIds),
+  getContainersCount: (monitorId, filter) => ipcRenderer.invoke('getContainersCount', monitorId, filter),
 });
