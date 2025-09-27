38bb0d2787484511b197bacc3058068371545752
Sam909001 <ademkaya1990kaya@gmail.com>
2025-09-27 18:15:07 +0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -4,26 +4,14 @@ contextBridge.exposeInMainWorld('electronAPI', {
     // --- Monitor ID ---
     setMonitorId: (callback) => ipcRenderer.on('set-monitor-id', (event, id) => callback(id)),
 
-    // --- Packages ---
-    fetchPackagesForContainers: ({ containerIds, monitorId }) =>
-        ipcRenderer.invoke('fetchPackagesForContainers', { containerIds, monitorId }),
-    fetchContainerDetails: ({ containerId, monitorId }) =>
-        ipcRenderer.invoke('fetchContainerDetails', { containerId, monitorId }),
-
-    // --- Shipping / Orders ---
-    getContainersCount: ({ monitorId, filter }) =>
-        ipcRenderer.invoke('getContainersCount', { monitorId, filter }),
-    fetchShippingPackages: ({ monitorId, filter }) =>
-        ipcRenderer.invoke('fetchShippingPackages', { monitorId, filter }),
-
-    // --- Stock Items ---
-    fetchStockItems: ({ monitorId, status }) =>
-        ipcRenderer.invoke('fetchStockItems', { monitorId, status }),
+    // --- Packages / Containers ---
+    addPackage: (packageData) => ipcRenderer.invoke('add-package', packageData),
+    fetchPackages: (monitorId) => ipcRenderer.invoke('fetch-packages', monitorId),
 
     // --- Print Barcode / Labels ---
-    printBarcode: (htmlContent) => ipcRenderer.send('printBarcode', htmlContent),
-    printLabel: (htmlContent) => ipcRenderer.send('printLabel', htmlContent),
+    printBarcode: (htmlContent) => ipcRenderer.invoke('print-barcode', htmlContent),
+    printLabel: (htmlContent) => ipcRenderer.invoke('print-barcode', htmlContent), // same as barcode for now
 
-    // --- Misc ---
+    // --- Misc Notifications ---
     showNotification: (title, body) => ipcRenderer.send('showNotification', { title, body }),
 });
