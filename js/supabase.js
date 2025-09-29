619e8f9c5d72b4fc275a1ab6221c30da54d48e37
Sam909001 <ademkaya1990kaya@gmail.com>
2025-09-30 02:31:19 +0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -421,12 +421,10 @@ function generateUUID() {
 
 
 
-// EmailJS initialization
-(function() {
-    // EmailJS kullanıcı ID'si - KENDİ ID'NİZİ EKLEYİN
-    emailjs.init("jH-KlJ2ffs_lGwfsp");
-})();
-
+function isValidEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 // Elementleri bir defa tanımla
 const elements = {};
 
