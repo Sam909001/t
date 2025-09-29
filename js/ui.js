710e4b03ee3b89b4d68cde74fff52830f59c64d0
Sam909001 <ademkaya1990kaya@gmail.com>
2025-09-30 02:31:48 +0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -156,11 +156,7 @@ function validateFormDebounced(inputs, callback) {
         if (callback) callback(isValid);
     }, 200);
 }
-        
-function isValidEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
+
 
 // API anahtarı modalını göster
 function showApiKeyModal() {
