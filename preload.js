3337c7811bd19172e80c6052e6b3b3db19ec2294
Sam909001 <ademkaya1990kaya@gmail.com>
2025-10-07 16:15:18 +0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,17 +1,38 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld('electronAPI', {
-    // --- Monitor ID ---
-    setMonitorId: (callback) => ipcRenderer.on('set-monitor-id', (event, id) => callback(id)),
+  // Secure Storage
+  storeSet: (key, value) => ipcRenderer.invoke('store-set', key, value),
+  storeGet: (key) => ipcRenderer.invoke('store-get', key),
+  storeDelete: (key) => ipcRenderer.invoke('store-delete', key),
+  storeClearAll: () => ipcRenderer.invoke('store-clear-all'),
 
-    // --- Packages / Containers ---
-    addPackage: (packageData) => ipcRenderer.invoke('add-package', packageData),
-    fetchPackages: (monitorId) => ipcRenderer.invoke('fetch-packages', monitorId),
+  // Printing
+  getPrinters: () => ipcRenderer.invoke('get-printers'),
+  printToSpecificPrinter: (htmlContent, printerName) => 
+    ipcRenderer.invoke('print-to-specific-printer', htmlContent, printerName),
+  printBarcode: (htmlContent) => 
+    ipcRenderer.invoke('print-barcode', htmlContent),
+  testPrint: (htmlContent, printerName) => 
+    ipcRenderer.invoke('test-print', htmlContent, printerName),
+  debugPrinters: () => ipcRenderer.invoke('debug-printers'),
 
-    // --- Print Barcode / Labels ---
-    printBarcode: (htmlContent) => ipcRenderer.invoke('print-barcode', htmlContent),
-    printLabel: (htmlContent) => ipcRenderer.invoke('print-barcode', htmlContent), // same as barcode for now
+  // File System
+  saveExcelToNetwork: (excelData, filename) => 
+    ipcRenderer.invoke('save-excel-to-network', excelData, filename),
+  saveExcelLocally: (excelData, filename) => 
+    ipcRenderer.invoke('save-excel-locally', excelData, filename),
+  openFileLocation: (filePath) => 
+    ipcRenderer.invoke('open-file-location', filePath),
+  testNetworkConnection: () => 
+    ipcRenderer.invoke('test-network-connection'),
 
-    // --- Misc Notifications ---
-    showNotification: (title, body) => ipcRenderer.send('showNotification', { title, body }),
+  // App Info
+  getAppVersion: () => ipcRenderer.invoke('get-app-version'),
+  getAppPath: () => ipcRenderer.invoke('get-app-path')
 });
+
+// Log for debugging
+console.log('✅ Preload script loaded - Electron API available');
