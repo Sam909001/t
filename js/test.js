34beb10c33b986b8683b20a9e452e2b22d5ec8ea
Sam909001 <ademkaya1990kaya@gmail.com>
2025-10-07 16:12:39 +0300

    Delete js/test.js

--- js/test.js
+++
@@ -1,7 +0,0 @@
-const db = require('./database');
-
-// Insert a user
-const insert = db.prepare('INSERT INTO users (name, email) VALUES (?, ?)');
-insert.run('Sam', 'sam@example.com');
-
-console.log('User added!');
